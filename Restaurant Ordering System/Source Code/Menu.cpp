8a52dac09213097b3f81f7c24b8721be0a3dab49
Ra-cloud08 <thounsoktheara60@gmail.com>
2026-06-19 14:49:58 +0700

    Project-Y1-Rupp

---
+++ Restaurant Ordering System/Source Code/Menu.cpp
@@ -0,0 +1,58 @@
+#include"../Header/Menu.h"
+
+Menu::Menu(){
+
+    //Food
+    menuList.push_back({1,"Fried Rice: ",2.50,10000,"Food"});
+    menuList.push_back({2,"Chicken Noodle Soup: ",2,8000,"Food"});
+    menuList.push_back({3,"Beef Lok Lak Rice: ",3.25,13000,"Food"});
+    menuList.push_back({4,"Grilled Pork Rice:" ,2.50,10000,"Food"});
+    menuList.push_back({5,"Khmer Curry: ",2.75,11000,"Food"});
+
+    //Drink
+    menuList.push_back({1,"Pepsi: ",1,4000,"Drink"});
+    menuList.push_back({2,"Cocacola: ",1.25,5000,"Drink"});
+    menuList.push_back({3,"Fresh Orange Juice: ",1.75,7000,"Drink"});
+    menuList.push_back({4,"Hot tea: ",1.25,5000,"Drink"});
+    menuList.push_back({5,"Beer: ",3.00,12000,"Drink"});
+}
+
+    void Menu::displayMenu(){
+        std::cout<<"\n========== Food ==========\n";
+            for(auto item:menuList){
+                if(item.category == "Food"){
+                    std::cout<<item.id
+                             <<","
+                             <<item.name
+                             <<":$"
+                             << std::fixed << std::setprecision(2)
+                             << item.usdPrice
+                             << " = "
+                             << item.khPrice
+                             << ":៛";
+                }
+            }
+        std::cout<<"\n========== Drink ==========\n";
+            for(auto item:menuList){
+                if(item.category == "Drink"){
+                    std::cout<<item.id
+                             <<","
+                             <<item.name
+                             <<":$"
+                             <<std::fixed << std::setprecision(2)
+                             <<item.usdPrice
+                             <<" = "
+                             <<item.khPrice
+                             <<":៛";
+                }
+            }
+        }
+MenuItem* Menu::finditem(int id, std::string category){
+    for(auto & item:menuList){
+        if(item.id == id && item.category == category){
+            return &item;
+        }
+    }
+
+    return nullptr;
+}
