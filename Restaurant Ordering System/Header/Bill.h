f99b1355ac52b60fbf654236459536410bbe3f94
mengkong2218-max <mengkong2218@gmail.com>
2026-06-19 17:20:04 +0700

    ber min der sur Ai klun eng tv

--- Restaurant Ordering System/Header/Bill.h
+++ Restaurant Ordering System/Header/Bill.h
@@ -2,24 +2,87 @@
 #define BILL_H
 
 #include <iostream>
-#include <thread>
-#include <format>
-#include <chrono> 
 #include <string>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+#include <ctime>
 
-inline void wait_for(unsigned int second) {
-   std::this_thread::sleep_for(std::chrono::seconds(second));
-}
+struct MenuItem{
+   int id;
+   std::string name;
+   double usdPrice;
+   int khPrice;
+   std::string category;
+};
 
-inline void print_Date() {
-   auto now = std::chrono::system_clock::now();
-   auto local_now = std::chrono::current_zone()->to_local(now);
-   std::string formatted = std::format("{:%Y-%m-%d %H:%M:%S}", std::chrono::floor<std::chrono::seconds>(local_now));
-   std::cout << "Current Datetime: " << formatted << std::endl;
-}
+class Bill {
+   private: 
+      std::vector<MenuItem> drinks;
+      std::vector<MenuItem> foods;
 
-inline void Log(std::string prompt) {
-   std::cout << prompt << std::endl;
-}
+      std::string get_date() {
+         std::time_t now = std::time(nullptr);
+         std::tm* local_time = std::localtime(&now); 
+
+         std::stringstream ss;
+         ss << std::put_time(local_time, "%Y-%m-%d %H:%M:%S");
+         return ss.str();
+      }
+
+      void apply_food_and_drink(std::vector<MenuItem>& fad, const MenuItem& item) {
+         fad.push_back(item);
+      }
+
+      void display_item(std::vector<MenuItem>& menu) {
+         int i = 1;
+         for (const auto& item : menu) {
+            std::cout << i << ". " << std::left << std::setw(30) << item.name << "$ " << std::left << std::setw(4) << std::setprecision(2) << item.usdPrice << " : r" << item.khPrice << std::endl;
+            i++;
+         }
+      }
+
+      double total_usd_price(std::vector<MenuItem>& menu) {
+         double sum = 0;
+         for (auto& item : menu) {
+            sum += item.usdPrice;
+         }
+         return sum;
+      }
+
+      unsigned long total_kh_price(std::vector<MenuItem>& menu) {
+         unsigned long sum = 0;
+         for (auto& item : menu) {
+            sum += item.khPrice;
+         } return sum;
+      }
+
+      void total_price_output() {
+         unsigned long total_kh = total_kh_price(foods) + total_kh_price(drinks);
+         double total_usd = total_usd_price(foods) + total_usd_price(drinks);
+         std::cout << "  " << std::left << std::setw(30) << "Total:" << "$ " << std::left << std::setw(4) << std::setprecision(2) << total_usd << " : r" << total_kh << std::endl;
+      }
+   public: 
+   Bill(std::vector<MenuItem>& fnd) {
+      for (const auto& item : fnd) {
+         if (item.category == "Food") {
+            apply_food_and_drink(foods, item);
+         } else if (item.category == "Drink") {
+            apply_food_and_drink(drinks, item);
+
+         }
+      }
+   }
+      void get_bill() {
+         std::cout << "Bill : " << get_date() << std::endl;
+         std::cout << "======================> Foods <=======================" << std::endl;
+         display_item(foods); 
+         std::cout << "======================> Drinks <======================" << std::endl;
+         display_item(drinks);
+         std::cout << "======================> Total <=======================" << std::endl;
+         total_price_output();
+      }
+
+};
 
 #endif
--- Restaurant Ordering System/Source Code/Bill.cpp
+++ Restaurant Ordering System/Source Code/Bill.cpp
@@ -3,9 +3,21 @@
 using namespace std;
 
 int main() {
-   print_Date();
-   cout << "Wait for another date:" << endl;
-   wait_for(3);
-   print_Date();
+   vector<MenuItem> menuList;
+   menuList.push_back({1,"Fried Rice: ",2.50,10000,"Food"});
+   menuList.push_back({2,"Chicken Noodle Soup: ",2,8000,"Food"});
+   menuList.push_back({3,"Beef Lok Lak Rice: ",3.25,13000,"Food"});
+   menuList.push_back({4,"Grilled Pork Rice:" ,2.50,10000,"Food"});
+   menuList.push_back({5,"Khmer Curry: ",2.75,11000,"Food"});
+
+   //Drink
+   menuList.push_back({1,"Pepsi: ",1,4000,"Drink"});
+   menuList.push_back({2,"Cocacola: ",1.25,5000,"Drink"});
+   menuList.push_back({3,"Fresh Orange Juice: ",1.75,7000,"Drink"});
+   menuList.push_back({4,"Hot tea: ",1.25,5000,"Drink"});
+   menuList.push_back({5,"Beer: ",3.00,12000,"Drink"});
+
+   Bill myBill(menuList);
+   myBill.get_bill();
    return 0;
 }
